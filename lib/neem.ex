2d32b339e4a3f156b18ae272f4d769ffd21e55fa
Shankar Dhanasekaran <shankar@opendrops.com>
2017-06-15 16:07:36 +0530

    initial commit

---
+++ lib/neem.ex
@@ -0,0 +1,18 @@
+defmodule Neem do
+  @moduledoc """
+  Documentation for Neem.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Neem.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/neem_test.exs
@@ -0,0 +1,8 @@
+defmodule NeemTest do
+  use ExUnit.Case
+  doctest Neem
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
